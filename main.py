969d405ad0d2d940db90f60529c8a07b4f2e0314
paul-raja-dev <paulraja.creator@gmail.com>
2025-12-31 21:41:00 +0530

    function - add_student : base-commit

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+def add_student():
+    print('--------------Addition of new student------------------')
+    reg_no=input('Enter the register number of the new student : ')    
+    name=input('Enter the name of the new student : ')
+    marks=input('Enter the marks of the student : ')
+    data=['\n',reg_no,',',name,',',marks]
+    with open('data.txt','a') as f:
+        f.writelines(data)
+
+
+
+
 def main():
     while True:
         print('\n1. Add student' 
@@ -6,7 +18,7 @@ def main():
         '\n4. Delete student' 
         '\n5. Search student' 
         '\n6. Exit')
-        choice=int(input('Enter the choice'))
+        choice=int(input('Enter the choice : '))
         if choice==1:
             add_student()
         elif choice==2:
@@ -18,6 +30,8 @@ def main():
         elif choice==5:
             search_student()
         elif choice==6:
+            print('Exiting from the software ...')
+            print('Exited successfully')
             break
         else:
             print('Please Enter from the choice')
