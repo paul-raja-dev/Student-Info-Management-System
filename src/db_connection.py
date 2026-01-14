75a98d0dddfe157f12cceb54d90037a326230d74
paul-raja-dev <paulraja.creator@gmail.com>
2026-01-14 12:09:27 +0530

    make connection as function get_connection

--- src/db_connection.py
+++ src/db_connection.py
@@ -4,19 +4,17 @@ import MySQLdb
 
 load_dotenv()
 
-conn=MySQLdb.connect(
-    host=os.getenv('DB_HOST'),
-    user=os.getenv('DB_USER'),
-    passwd=os.getenv('DB_PASSWORD'),
-    db=os.getenv('DB_NAME')
-)
-
-cur=conn.cursor()
-
-cur.execute("SHOW TABLES;")
-table=cur.fetchall()
-for i in table:
-    print(i)
-
-cur.close()
-conn.close()
+def get_connection():
+    try:
+        conn=MySQLdb.connect(
+           host=os.getenv('DB_HOST'),
+           user=os.getenv('DB_USER'),
+           passwd=os.getenv('DB_PASSWORD'),
+           db=os.getenv('DB_NAME')
+        )
+        return conn
+        
+    except Exception as e:
+        print('Error connecting with the Database',e)
+        return None
+ 
